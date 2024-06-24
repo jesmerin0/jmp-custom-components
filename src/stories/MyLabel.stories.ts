d78d778b239336b3355765183c0f0cf958e5e7bc
Merino Perez <869450@cognizant.com>
2024-06-24 14:26:22 +0200

    storybook init

---
+++ src/stories/MyLabel.stories.ts
@@ -0,0 +1,47 @@
+import { MyLabel } from '../components/MyLabel';
+import type { Meta, StoryObj } from '@storybook/react';
+
+
+const meta = {
+    title: 'UI/labels/MyLabel',
+    component: MyLabel,
+    tags: ['autodocs'],
+    parameters: {
+        // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
+        layout: 'centered',
+      },
+    argTypes: {
+        size: { control: 'inline-radio'},
+        // size: { control: 'select'},
+        fontColor: { control: 'color' },
+    }
+} satisfies Meta<typeof MyLabel>;
+
+export default meta;
+type Story = StoryObj<typeof meta>
+
+export const Basic: Story = {
+    args:{
+      label: 'Basic label',
+    }
+  };
+
+export const AllCaps: Story = {
+    args:{
+      label: 'All Caps label',
+      allCaps: true,
+    }
+  };
+  export const Secondary: Story = {
+    args:{
+      label: 'Secondary label',
+      color: 'text-secondary',
+    }
+  };
+
+  export const CustomColor: Story = {
+    args:{
+      label: 'Custom color label',
+      fontColor: '#5517ac',
+    }
+  };
